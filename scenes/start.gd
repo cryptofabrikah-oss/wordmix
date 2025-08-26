a9c0164a6ea4c1566366115af5d2d4316a0ea48a
Fabrikah <cryptofabrikah@gmail.com>
2025-08-26 03:04:58 +0000

    start menu update

--- scenes/start.gd
+++ scenes/start.gd
@@ -1,34 +1,41 @@
 extends Control
 
-@onready var start_btn = $VBoxContainer/StartB
+# TopBar
+@onready var gold_line = $TopBar/gold_line
+@onready var crystal_line = $TopBar/crystal_line
+# CenterBox
+@onready var start_btn = $CenterBox/StartB
+@onready var playerselect_btn = $CenterBox/PLayerB
+# BottomBar
 @onready var market_btn = $BottonBar/Market
 @onready var rank_btn = $BottonBar/Rank
 @onready var inventory_btn = $BottonBar/Inventory
-@onready var playerselect_btn = $VBoxContainer/PLayerB
 
 func _ready():
+	# Conecta os sinais
 	market_btn.pressed.connect(_on_market_pressed)
 	rank_btn.pressed.connect(_on_rank_pressed)
 	inventory_btn.pressed.connect(_on_inventory_pressed)
 	start_btn.pressed.connect(_on_start_pressed)
 	playerselect_btn.pressed.connect(_on_player_pressed)
 
-#novo jogo
+
+# Novo jogo
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://scenes/Main.tscn")
 
-#ranking semanal
+# Seleção de jogador
+func _on_player_pressed():
+	get_tree().change_scene_to_file("res://scenes/Main.tscn")
+
+# Ranking semanal
 func _on_rank_pressed():
 	get_tree().change_scene_to_file("res://scenes/Rank.tscn")
 
-#marketplace
+# Marketplace
 func _on_market_pressed():
 	get_tree().change_scene_to_file("res://scenes/Main.tscn")
 
-#inventory	
+# Inventory
 func _on_inventory_pressed():
 	get_tree().change_scene_to_file("res://scenes/Main.tscn")
-
-func _on_player_pressed():
-	get_tree().change_scene_to_file("res://scenes/Main.tscn")
-	
